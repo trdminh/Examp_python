454c735161b2c54e09483b49ff0b6dba34ad2cf9
trdminh <131636385+trdminh@users.noreply.github.com>
2023-09-26 11:11:37 +0700

    Add files via upload
    Signed-off-by: trdminh <131636385+trdminh@users.noreply.github.com>

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -1,5 +1,4 @@
 import os
-import nltk
 import ssl
 import streamlit as st
 import random
@@ -7,8 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
 ssl._create_default_https_context = ssl._create_unverified_context
-nltk.data.path.append(os.path.abspath("nltk_data"))
-nltk.download('punkt')
+
 
 intents = [
     {
