5141b7de9862b6addaa0775bc33ab2026ca713ec
trdminh <131636385+trdminh@users.noreply.github.com>
2023-09-26 11:13:47 +0700

    Update chatbot.py
    Signed-off-by: trdminh <131636385+trdminh@users.noreply.github.com>

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -1,12 +1,14 @@
 import os
 import ssl
+import nltk
 import streamlit as st
 import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 
 ssl._create_default_https_context = ssl._create_unverified_context
-
+nltk.data.path.append(os.path.abspath("nltk_data"))
+nltk.download('punkt')
 
 intents = [
     {
